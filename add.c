aea98b10877f0d166fccd0c12d8de6e1c10e9591
HAWKEYE_368 <56731818+Biswajit368@users.noreply.github.com>
2022-10-03 18:37:20 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int A, B, sum = 0;
+	printf("Enter two numbers A and B : \n");
+  A = 2, B = 3
+	scanf("%d%d", &A, &B);
+  sum = A + B;
+  printf("Sum of A and B is: %d", sum);
+  return 0;
+}
